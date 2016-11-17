0ee52af5f425a799cf3a605c67613ff4de2d9c27
proharvesting <jjnavarromillan@gmail.com>
2016-11-16 17:23:39 -0800

    Initial commit

---
+++ app/src/main/java/com/example/proharvesting/basiclistviewapplication/ListViewActivity.java
@@ -0,0 +1,13 @@
+package com.example.proharvesting.basiclistviewapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+    }
+}
